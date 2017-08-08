cb0098de7fdfa34673056df8a239f9375151b058
Aleksei Triastcyn <aleksei.triastcyn@epfl.ch>
2017-08-08 11:14:42 +0200

    Update

--- iccluster_setup.sh
+++ iccluster_setup.sh
@@ -17,20 +17,17 @@ sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 apt-get update
 apt-get --assume-yes install cuda
 
-# download and install libcudnn5, which is necessary, for example, for Conv2D layers
-# not needed for pytorch
-:'
-ML_REPO_PKG=http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb
-wget "$ML_REPO_PKG" -O /tmp/ml-repo.deb && sudo dpkg -i /tmp/ml-repo.deb && rm -f /tmp/ml-repo.deb
-apt-get update
-apt-get --assume-yes install libcudnn5
-'
-
 # install python packages for machine learning
 yes | pip2 install --upgrade pip
 yes | pip3 install --upgrade pip
 yes | pip2 install pillow matplotlib mpmath jupyter keras sklearn tensorflow tensorflow-gpu
 yes | pip3 install pillow matplotlib mpmath jupyter keras sklearn tensorflow tensorflow-gpu
+
+# install pytorch
+pip2 install http://download.pytorch.org/whl/cu80/torch-0.1.2.post1-cp27-cp27m-manylinux1_x86_64.whl
+pip2 install torchvision
+pip3 install http://download.pytorch.org/whl/cu80/torch-0.1.2.post1-cp35-cp35m-manylinux1_x86_64.whl
+pip3 install torchvision
 
 # clean up
 cd /home
