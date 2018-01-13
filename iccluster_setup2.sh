d87fcf2f7dbbca9623eebe9719abcbe4183123d7
MiFei <fei.mi@epfl.ch>
2018-01-13 21:23:38 +0100

    Update iccluster_setup2.sh

--- iccluster_setup2.sh
+++ iccluster_setup2.sh
@@ -4,7 +4,9 @@
 apt-get update
 apt-get --assume-yes upgrade
 
-
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+sudo dpkg-reconfigure locales
 
 # install python and tools
 apt-get --assume-yes install nano
@@ -13,8 +15,14 @@ apt-get --assume-yes install python3 python3-dev python3-pip
 mkdir /home/downloads
 cd /home/downloads
 
-# download and install CUDA
-sudo apt-get install cuda-8.0
+# # download and install CUDA
+# sudo apt-get install cuda-8.0
+
+curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+apt-get update
+#  Added this to make sure we don't drag down the newest version of cuda!
+apt-get install cuda=8.0.61-1 -y
 
 # download and install libcudnn6, which is necessary tensorflow 1.4 GPU
 CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
