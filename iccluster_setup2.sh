7b65d1f2868e3ef914b5507c00b4f4dcfb7f2f4f
MiFei <fei.mi@epfl.ch>
2018-04-12 17:12:40 +0200

    Update iccluster_setup2.sh

--- iccluster_setup2.sh
+++ iccluster_setup2.sh
@@ -55,5 +55,6 @@ rm -r ./downloads
 
 # set environment variables
 export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/extras/CUPTI/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export MKL_THREADING_LAYER=GNU
